0b26be3890140cb03df992d6dbaaf15a55cacb08
superDoss <shbtov@gmail.com>
2019-03-01 12:50:01 +0200

    assure that filename is uniqe

--- routes/files.js
+++ routes/files.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+//TODO: Change to basedir
 const conf = require('../config');
-const multer = require('multer');
-const upload = multer({dest:conf.uploadsPath});
+const path = require('path');
 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination:conf.uploadsPath,
+    // Make sure file name is uniqe
+    filename:(req,file,cb) => {
+        cb(null,Math.random().toString(36).substr(2, 9) + `_${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage:storage });
+
+//TODO: Change to basedir
 const FilesController = require('../src/controllers/files');
 const filesController = new FilesController();
 
